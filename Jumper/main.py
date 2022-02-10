9b3c7cbf974103aee23d7559b89cb8c5c9eb25d0
GhostLeader119 <97194393+GhostLeader119@users.noreply.github.com>
2022-02-10 11:23:17 -0700

    Update main.py
    Added option to allow you to replay the game without needing to restart the program

--- Jumper/main.py
+++ Jumper/main.py
@@ -1,4 +1,16 @@
 from Game.Directory import Director
+running_state = True
+while running_state:
+    director = Director()
+    director.start_game()
 
-director = Director()
-director.start_game()
+
+    while True:
+        choice = input('\nWould you like to play again? Y/N ').upper()
+        if choice == 'Y' or choice == 'YES':
+            break
+        elif choice == 'N' or choice == 'NO':
+            running_state = False
+            break
+        else:
+            print('\nEnter a valid input...')
